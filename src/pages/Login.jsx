4b39a8c335bf0c549f39e54d6ab1b51df410f0f3
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-10-07 16:46:50 +0500

    controlled forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import FacebookBtn from "../components/elements/FacebookBtn"
 import {Link} from 'react-router-dom'
 
 function Login() {
+  const initialState = {
+    email: '',
+    password: ''
+  }
+  const [formData, setFormData] = useState(initialState)
+
+  function updateFields(e){
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.table(formData)
+    setFormData(initialState)
+  }
+
   return (
     <div className="login container my-5">
       <div className="row rounded" style={{boxShadow: '1px 1px 10px 1px rgba(0,0,0,0.1)'}}>
@@ -17,24 +34,28 @@ function Login() {
           <p className="mb-5 mt-4 fw-bold text-decoration-underline">Login to your account</p>
           <FacebookBtn />
           <h6 className="opacity-50 my-4">__OR__</h6>
-          <form>
-            <div class="form-floating mb-3 mx-auto"
+          <form onSubmit={handleSubmit}>
+            <div className="form-floating mb-3 mx-auto"
                 style={{maxWidth: '300px'}}>
               <input
+                value={formData.email}
                 type="email"
+                onChange={updateFields}
                 required
-                class="form-control"
+                className="form-control"
                 id="email"
                 placeholder="name@example.com"
               />
               <label htmlFor="email">Email address</label>
             </div>
-            <div class="form-floating mb-3 mx-auto"
+            <div className="form-floating mb-3 mx-auto"
                 style={{maxWidth: '300px'}}>
               <input
+                value={formData.password}
                 type="password"
+                onChange={updateFields}
                 required
-                class="form-control"
+                className="form-control"
                 id="password"
                 placeholder="name@example.com"
               />
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import FacebookBtn from "../components/elements/FacebookBtn"
 import {Link} from 'react-router-dom'
 
 function Signup() {
+  const initialState = {
+    email: '',
+    username: '',
+    dob: '',
+    password: '',
+    cpassword: ''
+  }
+  const [formData, setFormData] = useState(initialState)
+
+  function updateFields(e){
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.table(formData)
+    setFormData(initialState)
+  }
+
   return (
     <div className="sign-up container my-5">
       <div className="row rounded" style={{boxShadow: '1px 1px 10px 1px rgba(0,0,0,0.1)'}}>
@@ -17,51 +37,59 @@ function Signup() {
           <p className="text-muted mb-5 mt-4 fw-bold text-decoration-underline">Create your account</p>
           <FacebookBtn />
           <h6 className="opacity-50 my-4">__OR__</h6>
-          <form>
-            <div class="form-floating mb-3 mx-auto"
+          <form onSubmit={handleSubmit}>
+            <div className="form-floating mb-3 mx-auto"
                 style={{maxWidth: '300px'}}>
               <input
+                value={formData.email}
+                onChange={updateFields}
                 type="email"
                 required
-                class="form-control"
+                className="form-control"
                 id="email"
                 placeholder="name@example.com"
               />
               <label htmlFor="email">Email address</label>
             </div>
-            <div class="form-floating mb-3 mx-auto"
+            <div className="form-floating mb-3 mx-auto"
                 style={{maxWidth: '300px'}}>
               <input
+                value={formData.username}
+                onChange={updateFields}
                 type="text"
                 required
-                class="form-control"
+                className="form-control"
                 id="username"
                 placeholder="name@example.com"
               />
               <label htmlFor="username">Username</label>
             </div>
-            <div class="form-floating mb-3 mx-auto"
+            <div className="form-floating mb-3 mx-auto"
                 style={{maxWidth: '300px'}}>
-              <input type="date" id="dob" className="form-control" required />
+              <input value={formData.dob} onChange={updateFields} type="date" id="dob" className="form-control" required />
               <label htmlFor="dob" className="mb-5">Your date of birth</label>
             </div>
-            <div class="form-floating mb-3 mx-auto"
+            <div className="form-floating mb-3 mx-auto"
                 style={{maxWidth: '300px'}}>
               <input
+                value={formData.password}
+                onChange={updateFields}
                 type="password"
                 required
-                class="form-control"
+                className="form-control"
                 id="password"
                 placeholder="name@example.com"
               />
               <label htmlFor="password">Password</label>
             </div>
-            <div class="form-floating mb-3 mx-auto"
+            <div className="form-floating mb-3 mx-auto"
                 style={{maxWidth: '300px'}}>
               <input
+                value={formData.cpassword}
+                onChange={updateFields}
                 type="password"
                 required
-                class="form-control"
+                className="form-control"
                 id="cpassword"
                 placeholder="name@example.com"
               />
